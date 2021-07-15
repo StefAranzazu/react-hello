7cf860045e799506193ffd0aa9f18611d4589e23
StefAranzazu <stefanyaranzazu1310@gmail.com>
2021-07-15 20:38:09 +0000

    add jumbotron

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export function Jumbotron() {
-	return <div>jumbotron</div>;
+	return (
+		<div>
+			<div className="jumbotron jumbotron-fluid">
+				<div className="container">
+					<h1 className="display-4">Fluid jumbotron</h1>
+					<p className="lead">
+						This is a modified jumbotron that occupies the entire
+						horizontal space of its parent.
+					</p>
+				</div>
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Learn more
+				</a>
+			</div>
+		</div>
+	);
 }
